57fd0a661ea02856b8ad31fc5e2d101be526e731
kondo takeshi <chaspy@users.noreply.github.com>
2018-12-28 18:41:52 +0900

    Concatenate url

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ fn main() {
   extern crate reqwest;
   use std::io;
 
-  let endpoint = "https://api.github.com/repos/chaspy/favsearch/issues";
-  let mut res = reqwest::get(endpoint).unwrap();
+  let endpoint = "https://api.github.com".to_string();
+  let org = "chaspy";
+  let repo = "favsearch";
+  let url = endpoint + "/" + "repos" + "/" + org + "/" + repo + "/" + "issues";
+
+  let mut res = reqwest::get(&*url).unwrap();
   res.copy_to(&mut io::stdout()).unwrap();
 }
