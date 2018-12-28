f54d6317a442e649d3851ffaaa7a23b4b24ae64a
kondo takeshi <chaspy@users.noreply.github.com>
2018-12-28 18:17:58 +0900

    Get issues without token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+  extern crate reqwest;
+  use std::io;
+
+  let endpoint = "https://api.github.com/repos/chaspy/favsearch/issues";
+  let mut res = reqwest::get(endpoint).unwrap();
+  res.copy_to(&mut io::stdout()).unwrap();
 }
