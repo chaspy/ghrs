d0078d7490789634ec5a310551e7c5b78024a985
kondo takeshi <chaspy@users.noreply.github.com>
2019-06-01 23:46:11 +0900

    Get issues from issues_repos

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 	"os"
+	"strings"
 )
 
 var (
@@ -28,6 +29,21 @@ func main() {
 	tc := oauth2.NewClient(ctx, ts)
 
 	client := github.NewClient(tc)
+
+	issue_repos_slice := strings.Split(issue_repos, " ")
+	for _, repo := range issue_repos_slice {
+		fmt.Println(repo)
+		repos, _, err := client.Issues.List(ctx, true, nil)
+
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return
+		}
+
+		for i, repo := range repos {
+			fmt.Printf("%#v. %+v\n", i+1, repo.GetURL())
+		}
+	}
 
 	// list all repositories for the authenticated user
 	repos, _, err := client.Repositories.List(ctx, owner, nil)
