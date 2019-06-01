7f18ce8d62d7b087a1b7b45481a2ae0bd69f35f0
kondo takeshi <chaspy@users.noreply.github.com>
2019-06-01 22:39:57 +0900

    List repositories

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func FetchOrganizations(username string) ([]*github.Organization, error) {
@@ -17,13 +19,23 @@ func main() {
 	fmt.Print("Enter GitHub username: ")
 	fmt.Scanf("%s", &username)
 
-	organizations, err := FetchOrganizations(username)
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
 
-	for i, organization := range organizations {
-		fmt.Printf("%v. %v\n", i+1, organization.GetLogin())
+	for i, repo := range repos {
+		fmt.Printf("%#v. %+v\n", i+1, repo.GetURL())
 	}
 }
